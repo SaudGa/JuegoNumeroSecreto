6e288169cfcb86a1d18079ba1595f5db58b86443
Diego Saud <diegosaud282@gmail.com>
2024-11-19 17:37:13 -0300

    modelo mejorado

--- app.js
+++ app.js
@@ -3,11 +3,26 @@ let numeroIntentos = 0;
 let listaNumerosSorteados = [];
 let numeroMaximo = 10;
 
+//Esta es la función que me permite interactuar con el <h1> y el <p> y de esta manera hacer más dinamico el juego
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
 }
 
+//Al refrescarse la página estás son las condiciones iniciales de los elementos HTML
+function condicionesIniciales(){
+    numeroSecreto = generarNumeroSecreto();
+    asignarTextoElemento("h1", "Juego del número secreto");
+    asignarTextoElemento("p", `Elige un número entre el 1 y el ${numeroMaximo}`);
+    numeroIntentos = 1;
+}
+
+function limpiarCaja(){
+    document.getElementById("numeroUs").value = ""
+    
+}
+
+//Aquí verificamos el intento del usuario
 function verificarIntento() {
     let numeroUsuario = parseInt(document.getElementById("numeroUs").value);
     if (numeroUsuario === numeroSecreto){
@@ -27,11 +42,7 @@ function verificarIntento() {
     return;
 }
 
-function limpiarCaja(){
-    document.getElementById("numeroUs").value = ""
-    
-}
-//Aquí aplicamos la recursividad (llamar una función dentro de la misma):
+//Aquí aplicamos recursividad para que el numero incognito no se repita
 function generarNumeroSecreto() {
     let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
 
@@ -49,13 +60,7 @@ function generarNumeroSecreto() {
    
 }
 
-function condicionesIniciales(){
-    numeroSecreto = generarNumeroSecreto();
-    asignarTextoElemento("h1", "Juego del número secreto");
-    asignarTextoElemento("p", `Elige un número entre el 1 y el ${numeroMaximo}`);
-    numeroIntentos = 1;
-}
-
+//Función que le asignamos al botón "Nuevo juego"
 function reiniciarJuego() {
     limpiarCaja();
     condicionesIniciales();
